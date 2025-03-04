2e0661c5ba17e5996c5e5c01d1c460f310e7b9cb
Unknown9625 <ccmiller96@protonmail.com>
2025-03-03 21:37:38 -0500

    Changed command-line arguments and finished my first ever mini-python project

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 # main.py
 
-from stats import get_num_words # Imports a function that was created in another document
-from stats import get_character_count 
-from stats import sorted_list
+import sys
+
+from stats import get_num_words, get_character_count, sorted_list  # Imports a function that was created in another document
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+path = sys.argv[1]
 
 def get_book_text(path):
     with open(path) as f: # The 'with' statement creates a context manager that automatically closes the file when done
         return f.read() # f.read() reads the entire file content using a single string
 
 def main():
-    path = "books/frankenstein.txt" # This defines the relative path and tells Python to find a file starting from the current directory where the script is run, rather than using an absolute location in your file system
     text = get_book_text(path) # Calls back our get_book_text function to retrieve the content of the book
     num_words = get_num_words(text) 
     
